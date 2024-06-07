125e775b08e7cb246a730f5e8f142aaa55a2f619
iag-lol <isaac_avila.gomez@outlook.com>
2024-06-07 17:25:12 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// JavaScript para añadir interactividad
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('IntraLog cargado correctamente');
+});// JavaScript para añadir interactividad
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('IntraLog cargado correctamente');
+});
